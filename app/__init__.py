acdf0696527a1f9d08b83b1f4496d260c5e51a4b
divesh <ddc.divesh@gmail.com>
2018-08-20 03:22:21 +0530

    restructured app for heroku

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,22 @@
 from flask import Flask
 from pymongo import MongoClient
+import os
 from flask import (
     render_template
 )
 
 def create_app(database_uri=None, debug=True):
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.debug = debug
+
     # connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True, connect=False, maxPoolsize=1
     client = MongoClient("mongodb://divesh:divesh123@ds123012.mlab.com:23012/typetest",connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True, connect=False, maxPoolsize=1)
     db = client.typetest
     speed_results = db.scores
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     @app.route('/')
     def hello():
@@ -19,7 +25,6 @@ def create_app(database_uri=None, debug=True):
 
     return app
 
-if __name__ == "__main__":
-    app = create_app()
-    # app = create_app(config.DATABASE_URI, debug=True)
-    app.run()
+# app = create_app()
+# app = create_app(config.DATABASE_URI, debug=True)
+# app.run()
