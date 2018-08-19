1df8008b63b05670e2f6d81c3c135645bdbceafe
Divesh Narayanan Naidu <divesh@Diveshs-MacBook-Air.local>
2018-08-20 01:52:09 +0530

    removed pymongo-flask for plain pymongo

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from flask_pymongo import PyMongo
-
+from pymongo import MongoClient
 from flask import (
     render_template
 )
@@ -8,21 +7,15 @@ from flask import (
 def create_app(database_uri=None, debug=True):
     app = Flask(__name__)
     app.debug = debug
+    # connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True, connect=False, maxPoolsize=1
+    client = MongoClient("mongodb://divesh:divesh123@ds123012.mlab.com:23012/typetest",connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True, connect=False, maxPoolsize=1)
+    db = client.typetest
+    speed_results = db.scores
 
-    app.config["MONGO_URI"] = "mongodb://divesh:divesh123@ds123012.mlab.com:23012/typetest"
-    mongo = PyMongo(app)
-    # set up your database
-    # app.engine = create_engine(database_uri)
-
-    # add your modules
-    # app.register_module(frontend)
-
-    # other setup tasks
     @app.route('/')
     def hello():
-        scores = mongo.db.scores.find({})
         return render_template("index.html",
-            scores= sorted(list(scores), key=lambda x : x['speed'],reverse=True))
+            scores= sorted(list(speed_results.find()), key=lambda x : x['speed'],reverse=True))
 
     return app
 
