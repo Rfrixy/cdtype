2c04d7ca0180a9ef7d8f61ef222b04f78969d057
divesh <ddc.divesh@gmail.com>
2018-08-27 03:55:02 +0530

    added sign up and login

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from pymongo import MongoClient
 import os
 from flask import (
-    render_template, request, session
+    render_template, request, session, redirect
 )
 from flask_mail import Mail, Message
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -39,7 +39,7 @@ def create_app(database_uri=None, debug=True):
 
     def sendLoginMail(mailid):
         with app.app_context():
-            pwdlink = "cdtype.herokuapp.com/?user=divesh.naidu@collegedunia.com&hash=%s"%generate_password_hash(mailid[::-1])
+            pwdlink = "cdtype.herokuapp.com/?user=%s&hash=%s"%(mailid,generate_password_hash(mailid[::-1]))
             msg = Message(subject="CDTYPE login details",
                           sender=app.config.get("MAIL_USERNAME"),
                           recipients=[mailid], # replace with your email for testing
@@ -71,17 +71,67 @@ def create_app(database_uri=None, debug=True):
         try:
             speed = float(speed)
         except:
-            return
+            speed = 0.0
         user = list(db.scores.find({'email':email}))[0]
-        old_speed = float(user['speed'])
+        try:
+            old_speed = float(user['speed'])
+        except:
+            old_speed = 0.0
+
         print ('found speed', old_speed)
         if speed > old_speed:
             db.scores.update({'email':email},{'$set':{'speed':speed,'achieved_on': datetime.datetime.now()}})
 
+
+    @app.route('/profile')
+    def profile():
+        return render_template('profile.html')
+
+    @app.route('/logout')
+    def logout():
+        session.clear()
+        return redirect("/", code=302)
+
+    @app.route('/login',methods=('GET', 'POST'))
+    def login():
+        if request.method == 'POST':
+            mail= request.form.get('email')
+            print ( mail, 'm')
+            if mail:
+                l = list(db.scores.find({'email':mail}))
+                if len(l) > 0:
+                    print(mail,'mf')
+                    sendLoginMail(mail)
+                    return render_template("login.html",success = True)
+                else:
+                    return render_template("login.html", error = True,success = False)
+
+        return render_template("login.html")
+
+    @app.route('/signup', methods=('GET', 'POST'))
+    def signup():
+        if request.method == 'POST':
+            name = request.form.get('name')
+            email = request.form.get('email')
+            dept = request.form.get('team')
+            print(email, "mila")
+            user = get_user(email)
+            if user:
+                return render_template("sign_up.html",text= True, duplicate=True)
+            if name and email and dept:
+                sendLoginMail(email)
+                db.scores.insert({'email': email, 'name':name, 'dept':dept})
+                return render_template("sign_up.html",text= True, success=True)
+            else:
+                return render_template("sign_up.html",error= True)
+
+        return render_template("sign_up.html")
+
     @app.route('/', methods=('GET', 'POST'))
     def hello():
+        #Admin user creation
         if request.method == 'POST':
-            if(request.form['pwd']=='cordova'):
+            if(request.form.get('pwd')=='cordova'):
                 username = request.form['name']
                 email = request.form['email']
                 dept = request.form['team']
@@ -97,11 +147,27 @@ def create_app(database_uri=None, debug=True):
                 session.clear()
                 session['user_mail'] = user['email']
 
+        if request.method == 'GET':
+            email = request.args.get('user')
+            if(email):
+                hash_result = check_password_hash(request.args.get('hash'),email[::-1])
+                print (hash_result,'hashres')
+                if hash_result:
+                    user = get_user(email)
+                    if user:
+                        session['user_mail'] = user['email']
+                        return redirect('/')
+                    else:
+                        session.clear()
+                else:
+                    session.clear()
+
         texts = db.texts.find();
         textlist = list(texts)
+        scores = [ x for x in list(speed_results.find()) if x.get('speed')]
         chosen_text = random.choice(textlist)['text']
         return render_template("index.html",
-            scores= sorted(list(speed_results.find()), key=lambda x : x['speed'],reverse=True),
+            scores= sorted(scores, key=lambda x : x.get('speed'),reverse=True),
             mailid = session.get('user_mail'),
             text = chosen_text)
 
